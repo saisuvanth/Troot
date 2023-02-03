c8ca27bb0c1400b62dce12943e8ca421d04de5e0
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2023-02-03 21:34:40 +0530

    Adding ground

---
+++ Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCoordinates : MonoBehaviour
+{
+    public static float xOffset=1f,yOffset=1,zOffset=0.86f;
+
+    [Header("Offset Coordinates")]
+    [SerializeField] 
+    private Vector3Int offsetCoordinates;
+
+    private void Awake()
+    {
+        offsetCoordinates = ConvertToOffsetCoordinates(transform.position);
+    }
+
+    private Vector3Int ConvertToOffsetCoordinates(Vector3 position)
+    {
+        int x = Mathf.CeilToInt(position.x / xOffset);
+        int y = Mathf.RoundToInt(position.y / yOffset);
+        int z = Mathf.RoundToInt(position.z / zOffset);
+        return new Vector3Int(x, y, z);
+    }
+}
