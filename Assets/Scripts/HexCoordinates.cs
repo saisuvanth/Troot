83a2e3107d8033bc4d47c3f2ea8d0bafc8875bb6
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2023-02-04 11:09:37 +0530

    Adding code

--- Assets/Scripts/HexCoordinates.cs
+++ Assets/Scripts/HexCoordinates.cs
@@ -8,21 +8,25 @@ public class HexCoordinates : MonoBehaviour
 
     internal Vector3Int GetHexCoords() => offsetCoordinates;
 
+    private GenerateMap generateMap;
+
     [Header("Offset Coordinates")]
     [SerializeField]
     private Vector3Int offsetCoordinates;
 
     private void Awake()
     {
+        generateMap = FindObjectOfType<GenerateMap>();
         offsetCoordinates = ConvertToOffsetCoordinates(transform.position);
     }
 
     private Vector3Int ConvertToOffsetCoordinates(Vector3 position)
     {
-        int x = Mathf.CeilToInt(position.x / xOffset);
-        int y = Mathf.RoundToInt(position.y / yOffset);
-        int z = Mathf.RoundToInt(position.z / zOffset);
-        return new Vector3Int(x, y, z);
+        return generateMap.WorldToOdd(position);
+        // int x = Mathf.CeilToInt(position.x / xOffset);
+        // int y = Mathf.RoundToInt(position.y / yOffset);
+        // int z = Mathf.RoundToInt(position.z / zOffset);
+        // return new Vector3Int(x, y, z);
     }
 
 
