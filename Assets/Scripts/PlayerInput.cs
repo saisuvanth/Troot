2e3a131ada9167797fb2f7d73bc73efe09485926
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2023-02-11 16:31:28 +0530

    Fixing the camera

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -18,10 +18,7 @@ public class PlayerInput : MonoBehaviour
 		const int LeftButton = 0;
 		if (Input.GetMouseButton(LeftButton))
 		{
-			// mouse movement in pixels this frame
 			Vector3 mouseDelta = Input.mousePosition - _prevMousePos;
-
-			// adjust to screen size
 			Vector3 moveDelta = mouseDelta * (360f / Screen.height);
 
 			_cam.Move(moveDelta.x, -moveDelta.y);
